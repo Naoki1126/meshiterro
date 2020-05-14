c20c1fe6539d4fbca7666f25c8ea592e15d844b1
Naoki1126 <m.naoki85@gmail.com>
2020-05-14 15:53:47 +0000

    firstcommit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '278c7adea9e43207ea06685f6df21957f397615a114af263aed03e6cc783b00eace5f82435667424734c5fd0d66a2801e0fbc36e30c5d09c43e234057d5248ed'
